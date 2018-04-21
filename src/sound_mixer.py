d1f0d35747ab6d75218d89bbce88d5599388b671
Sam Wasserman <sambwasserman@gmail.com>
2018-04-21 14:47:00 -0400

    modified

--- src/sound_mixer.py
+++ src/sound_mixer.py
@@ -6,34 +6,38 @@ import numpy as np
 devices = discover_devices(timeout=2)  # Default timeout is 5 seconds
 
 # Sound level we want to stay at
-DESIRED_AUDIO_LEVEL = 50
+DESIRED_AUDIO_LEVEL = 18
 
 #
-current_audio_level = 0
 
 # Record indefinately
-duration = 50
+duration = 1
 
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata) * 10
-    current_audio_level = volume_norm
-    print(int(volume_norm) * "|")
-
-
-def level_audio():
+def level_audio(indata, outdata, frames, time, status):
     # Run for the duration of the program
+    for device in devices:
+        volume_norm = np.linalg.norm(indata) * 100
+        current_audio_level = int(volume_norm)
+        print(current_audio_level * "|")
+        # Get each speakers current volume
+        volume = device.volume()
+        if current_audio_level < DESIRED_AUDIO_LEVEL:
+            print("INCREASING VOLUME")
+            device.set_volume(volume.actual + 5)
+        elif current_audio_level > DESIRED_AUDIO_LEVEL:
+            print("DECREASING VOLUME")
+            device.set_volume(volume.actual - 5)
+        else:
+            print("Volume is good!")
+
+
+main(audio_level):
+    DESIRED_AUDIO_LEVEL = audio_level
     while True:
-        for device in devices:
-            # Get each speakers current volume
-            volume = device.volume()
-            if current_audio_level < DESIRED_AUDIO_LEVEL:
-                device.set_volume(volume.actual + 5)
-            elif current_audio_level > DESIRED_AUDIO_LEVEL:
-                device.set_volume(volume.actual - 5)
+        with sd.Stream(callback=level_audio):
+            sd.sleep(duration * 1000)
 
 
 if __name__ == '__main__':
-    with sd.Stream(callback=print_sound):
-        sd.sleep(duration * 1000)
-        level_audio
+    main()
