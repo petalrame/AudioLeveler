7e35fe7aa40145f32b818a250842b8a376bb850b
Ram Patel <rr7patel@gmail.com>
2018-04-21 14:46:07 -0400

    Front end Changes

--- src/views.py
+++ src/views.py
@@ -10,6 +10,16 @@ def index():
     return render_template(
         'index.html')
 
+@app.route("/login")
+def login():
+    return render_template(
+        'login.html')
+
+@app.route("/set_volume", methods=["POST"])
+def setVolume():
+    volume = request.form["volumeSet"]
+    return volume
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
