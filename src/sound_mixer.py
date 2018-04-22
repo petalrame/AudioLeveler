5d9fd01c36981f4b1a78e5bdb13b06928563a1fb
Sam Wasserman <sambwasserman@gmail.com>
2018-04-22 09:17:22 -0400

    wah

--- src/sound_mixer.py
+++ src/sound_mixer.py
@@ -27,8 +27,9 @@ class VolumeController():
         # Normalize the data coming in from the microphone to calculate the current level
             U, s, Vh = np.linalg.svd(indata, full_matrices=False) # The top s-value x 100 seems to be roughly the device volume
             ml_num = s[0]*100
-            print("ML Num " + ml_num)
+            scaled_ml = ((100-0)/(self.max-self.min)) * (ml_num - self.max) + 100
             volume_norm = np.linalg.norm(indata) * 100
+
             self.num_iterations = self.num_iterations + 1
             if volume_norm > self.max:
                 self.max = volume_norm
@@ -40,27 +41,27 @@ class VolumeController():
             self.sd_total += sd_helper
             self.standard_deviation = math.sqrt((self.sd_total / self.num_iterations))
 
-            print("Max " + str(max))
-            print("Min " + str(min))
+            print("Max " + str(self.max))
+            print("Min " + str(self.min))
             print("Avg" + str(avg))
-            print("Standard deviation " + str(self.standard_deviation))
             scaled_value = ((100-0)/(self.max-self.min)) * (volume_norm - self.max) + 100
             print("Scaled value " + str(scaled_value))
             current_audio_level = int(scaled_value)
             print(int(current_audio_level))
             # Get each speakers current volume
             volume = device.volume()
+            if abs(int(scaled_value) - volume.actual) > abs(int(scaled_ml) - volume.actual):
+                current_audio_level = int(scaled_ml)
             print("Device VOLUME: ", volume.actual)
-            if current_audio_level < self.desired_audio_level:
+            if current_audio_level - 5 < self.desired_audio_level:
                 # Something sketchy happens where the audio comes in at 0
-                if current_audio_level is not 0:
+                if current_audio_level >= 8.0:
                     print("INCREASING VOLUME")
                     device.set_volume(volume.actual + 1)
-            elif current_audio_level > self.desired_audio_level:
+            elif current_audio_level + 5 > self.desired_audio_level:
                 print("DECREASING VOLUME")
                 device.set_volume(volume.actual - 1)
             else:
-                # relax for 5 seconds if the sound level is good
                 print("Sound is good!")
 
 
