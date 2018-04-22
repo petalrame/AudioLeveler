046ebf5aa2737b88979547962603209d7f465f89
Sam Wasserman <sambwasserman@gmail.com>
2018-04-22 09:41:21 -0400

    I hate the rain

--- src/views.py
+++ src/views.py
@@ -64,4 +64,4 @@ def handleVolControl(volume):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='174.225.10.150', port=port)
+    app.run(host='0.0.0.0', port=port)
