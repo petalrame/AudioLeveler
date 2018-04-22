f087799346e87eb672d4af460066efddd06f1540
Sam Wasserman <sambwasserman@gmail.com>
2018-04-22 09:32:46 -0400

    The rains down in Africa

--- src/views.py
+++ src/views.py
@@ -64,4 +64,4 @@ def handleVolControl(volume):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='smartleveler-api-heroku.herokuapp.com', port=port)
