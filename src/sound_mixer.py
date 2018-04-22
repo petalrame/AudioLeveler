d711b1e00a44496c480f3120b9c96dcdd1a45c8b
Sam Wasserman <sambwasserman@gmail.com>
2018-04-21 23:03:33 -0400

    fixed conventions

--- src/sound_mixer.py
+++ src/sound_mixer.py
@@ -5,49 +5,43 @@ import numpy as np
 import time
 import math
 
-total = 0
-max = 100
-num_iterations = 0
-min = 0
-sd_total = 0
-standard_deviation = 0
+
 class VolumeController():
     """Controls the volume of the speaker"""
     def __init__(self, desired_vol=50):
         self.desired_audio_level = desired_vol
         self.duration = 5
         self.devices = discover_devices(timeout=2)  # Default timeout is 5 seconds
+        self.total = 0
+        self.max = 100
+        self.num_iterations = 0
+        self.min = 0
+        self.sd_total = 0
+        self.standard_deviation = 0
 
 
     def level_audio(self, indata, outdata, frames, time, status):
         # Run for the duration of the program
-        global total
-        global max
-        global num_iterations
-        global min
-        global sd_total
-        global standard_deviation
-
         #for device in self.devices:
         device = soundtouch_device('192.168.1.105')  # Manual configuration
         # Normalize the data coming in from the microphone to calculate the current level
         volume_norm = np.linalg.norm(indata) * 100
-        num_iterations = num_iterations + 1
-        if volume_norm > max:
-            max = volume_norm
-        if volume_norm < min:
-            min = volume_norm
-        total = total + volume_norm
-        avg = total / num_iterations
+        self.num_iterations = self.num_iterations + 1
+        if volume_norm > self.max:
+            self.max = volume_norm
+        if volume_norm < self.min:
+            self.min = volume_norm
+        self.total = self.total + volume_norm
+        avg = self.total / self.num_iterations
         sd_helper = pow((volume_norm - avg),2)
-        sd_total += sd_helper
-        standard_deviation = math.sqrt((sd_total / num_iterations))
+        self.sd_total += sd_helper
+        self.standard_deviation = math.sqrt((self.sd_total / self.num_iterations))
 
         print("Max " + str(max))
         print("Min " + str(min))
         print("Avg" + str(avg))
-        print("Standard deviation " + str(standard_deviation))
-        scaled_value = ((100-0)/(max-min)) * (volume_norm - max) + 100
+        print("Standard deviation " + str(self.standard_deviation))
+        scaled_value = ((100-0)/(self.max-self.min)) * (volume_norm - self.max) + 100
         print("Scaled value " + str(scaled_value))
         current_audio_level = int(scaled_value)
         print(int(current_audio_level))
